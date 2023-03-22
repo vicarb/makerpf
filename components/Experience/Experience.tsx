e13904549984e190ae3a0695e09c2056e9099d97
vicarb <vcardenasb@protonmail.com>
2023-03-22 12:53:50 -0300

    layout v.0

--- components/Experience/Experience.tsx
+++ components/Experience/Experience.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['300', ],
+});
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ['300'],
+});
+
+
 
 export const Experience = () => {
   return (
     <div>
-    <h1 className='text-xl bold text-white'>Experiencia</h1>
-    <h1 className='text-white text-2xl'></h1>
-    <h1 className='text-white text-2xl'>
-    IOS/Android Apps
+    <h1 className={`${roboto.className} text-2xl text-white`}>Experiencia</h1>
+    <h1 className={`${inter.className} text-white text-xl`}>Ecommerce Deploy & Analytics</h1>
+    <h1 className={`${inter.className} text-white text-xl`}>IOS/Android Apps
     </h1>
-    <h1 className='text-white text-2xl'>
-    Chrome Extensions
+    <h1 className={`${inter.className} text-white text-xl`}>Chrome Extensions
     </h1>
     </div>
   )
